e3c38810c5eea0072b4c29f636c8a6534a9c084f
shotaNakamura-0517 <stin0517@gmail.com>
2023-02-11 20:28:09 +0900

    calcDeviationのソート処理を修正、一部リファクタリング実施、コメントを追加

--- practice/js/warikan.js
+++ practice/js/warikan.js
@@ -6,115 +6,150 @@ const WarikanResult = class {
     }
   }
 
-function warikan(paymentList){
-
-    let sum = 0;
-    let average = 0;
-    let deviationList;
-    let paymentMap = new Map();
-    let resultMap = new Map();
-
-    for (const elem of paymentList) {
-        let value = elem.payment;
-        sum += value;
-        if(paymentMap.has(elem.member)){
-            value += paymentMap.get(elem.member);
-        }
-        paymentMap.set(elem.member,value);
-    }
-    average = Math.round(sum / paymentMap.size);
-
-    deviationList = calcDeviation(average,paymentMap)
+  function warikan(paymentList){
+    /*
+    引数として、以下の形式の配列を想定
+      [{"member":"A" ,"payment":5000},
+        {"member":"B" ,"payment":5000},
+        {"member":"C" ,"payment":0},
+        {"member":"D" ,"payment":0},
+        {"member":"C" ,"payment":0},
+        {"member":"E" ,"payment":0
+      ]
+    */
     
-    for (let elem1 of deviationList.takeMoneyMemberList) {
+        let sum = 0;
+        let average = 0;
+        let deviationList;
+        let paymentMap = new Map();
+        let resultMap = new Map();
+    
+        //memberの支払額を合計して格納
+        for (const elem of paymentList) {
+            let value = elem.payment;
+            sum += value;
+            if(paymentMap.has(elem.member)){
+                value += paymentMap.get(elem.member);
+            }
+            paymentMap.set(elem.member,value);
+        }
+    
+        average = Math.round(sum / paymentMap.size);
+    
+        //一人当たりの支払額(average)と支払い済の金額の差額を計算
+        deviationList = calcDeviation(average,paymentMap)
+        
+        //支払い超過のmemberでloop
+        for (let elem1 of deviationList.takeMoneyMemberList) {
+            let i = 0;
+            let whileFlg = true;
+    
+            //memberの支払い超過分が解消されるまでloop(誰に返してもらうか確定するまで)
+            while(whileFlg && i < deviationList.giveMoneyMemberList.length){
+                const elem2 = deviationList.giveMoneyMemberList[i];
+                const resultMapKey = elem2.member;
+                let resultMapValue;
+                let result;
+                let calcResult = 0; 
+                let payment = 0;
+                calcResult = elem1.payment - elem2.payment;
+    
+                if(calcResult > 0){
+                //例）Aさん(1000円支払い超過) - Bさん(500円返済)の場合、true
+                    //例）AさんがBさんに支払う金額を格納(返り値のメッセージ用)
+                    payment = elem2.payment;
+                    //例）Aさんの支払い超過額を更新
+                    elem1.payment = calcResult;
+                    //例）Bさんの返済額を更新(支払い終えているので0円)
+                    elem2.payment = 0;
+                }else{
+                //例）Aさん(500円支払い超過) - Bさん(1000円返済)の場合、true
+                    //例）AさんがBさんに支払う金額を格納(返り値のメッセージ用)
+                    payment = elem1.payment;
+                    //例）Bさんの返済額を更新(500円)
+                    elem2.payment = - calcResult;
+                    //例）Aさんは支払超過分を解消できたので次のmemberの処理に進む
+                    whileFlg=false;
+                }
+    
+                if(resultMap.has(resultMapKey)){
+                    resultMapValue = resultMap.get(resultMapKey);
+                }else{
+                    resultMapValue = new Array();
+                }
+    
+                result = new WarikanResult(resultMapKey ,elem1.member, payment)
+                resultMapValue.push(result);
+                resultMap.set(resultMapKey,resultMapValue);
+    
+                i++;                  
+            }
+        }
+        let entries = resultMap.entries();
         let i = 0;
-        let whileFlg = true;
-        while(whileFlg && i < deviationList.giveMoneyMemberList.length){
-            let calcResult = 0; 
-            let payment = 0;     
-            calcResult = elem1.payment - deviationList.giveMoneyMemberList[i].payment;
-
-            if(calcResult > 0){
-                elem1.payment = calcResult;
-                payment = deviationList.giveMoneyMemberList[i].payment;
-                deviationList.giveMoneyMemberList[i].payment = 0;
-            }else{
-                deviationList.giveMoneyMemberList[i].payment = - calcResult;
-                payment = elem1.payment;
-                whileFlg=false;
-            }
-
-            if(resultMap.has(deviationList.giveMoneyMemberList[i].member)){
-                let arr = resultMap.get(deviationList.giveMoneyMemberList[i].member);
-                let result = new WarikanResult(deviationList.giveMoneyMemberList[i].member ,elem1.member, payment)
-                arr.push(result);
-                resultMap.set(deviationList.giveMoneyMemberList[i].member,arr);
-            }else{
-                let arr = new Array();
-                let result = new WarikanResult(deviationList.giveMoneyMemberList[i].member ,elem1.member, payment)
-                arr.push(result);
-                resultMap.set(deviationList.giveMoneyMemberList[i].member,arr);
-
-            }
-
-            i++;                  
-        }
-    }
-    let entries = resultMap.entries();
-    let j = 0;
-    let mapSize = resultMap.size;
-    let message='';
-    let end = '\n';
-
-    for (let entry of entries) {
-      for(let i = 0; i < entry[1].length; i++){
-          if(j == mapSize -1 && i == entry[1].length -1){
-              end = '';
+        let mapSize = resultMap.size;
+        let message='';
+        let end = '\n';
+    
+        //例）resultMapKeyでloop
+        for (let entry of entries) {
+          //MapValueの配列をloop(resultMapValue) 
+          for(let j = 0; j < entry[1].length; j++){
+              //最終行は行末に改行を追加しない
+              if(i == mapSize -1 && j == entry[1].length -1){
+                  end = '';
+              }
+              
+              //「~が~に0円支払う」のメッセージは作成しない
+              if(entry[1][j].payment !=0){
+                message +=  entry[1][j].from + 'が' + entry[1][j].to + 'へ'+ entry[1][j].payment + '円支払う' + end; 
+              }
           }
-          
-          if(entry[1][i].payment !=0){
-            message +=  entry[1][i].from + 'が' + entry[1][i].to + 'へ'+ entry[1][i].payment + '円支払う' + end; 
-          }
-      }
-      j++;
+          i++;
+        }
+        return message == '' ? '精算なし':message;
     }
-    return message == '' ? '精算なし':message;
-}
-
-function calcDeviation(average,paymentMap){
-    let entries = paymentMap.entries();
-    let takeMoneyMemberList = new Array();
-    let giveMoneyMemberList = new Array();
-
-    for (let entry of entries) {
-        let deviation = entry[1] - average;
-        if(deviation < 0){
-            giveMoneyMemberList.push({"member":entry[0],"payment":- deviation});
-        }else{
-            takeMoneyMemberList.push({"member":entry[0],"payment":deviation});
+    
+    function calcDeviation(average,paymentMap){
+        let entries = paymentMap.entries();
+        //支払い超過のmemberを格納
+        let takeMoneyMemberList = new Array();
+        //返済しなければいけないmemberを格納
+        let giveMoneyMemberList = new Array();
+    
+        for (let entry of entries) {
+            let deviation = entry[1] - average;
+            if(deviation < 0){
+                giveMoneyMemberList.push({"member":entry[0],"payment":- deviation});
+            }else{
+                takeMoneyMemberList.push({"member":entry[0],"payment":deviation});
+            }
         }
+        //降順でソート
+        takeMoneyMemberList.sort((a,b)=>{
+            if( a.payment > b.payment ){
+                return -1;
+            }else if( a.payment < b.payment ){
+                return 1;
+            } else{
+                return 0;
+            }
+        });
+      
+        //降順でソート
+        giveMoneyMemberList.sort((a,b)=>{
+            if( a.payment > b.payment ){
+                return -1;
+            }else if( a.payment < b.payment ){
+                return 1;
+            } else{
+                return 0;
+            }
+        });
+    
+        return{takeMoneyMemberList:takeMoneyMemberList,giveMoneyMemberList:giveMoneyMemberList};
     }
-    takeMoneyMemberList.sort((a,b)=>{
-        if( a.payment < b.payment ){
-            return -1;
-        }else if( a.payment > b.payment ){
-            return 1;
-        } else{
-            return 0;
-        }
-    });
-    giveMoneyMemberList.sort((a,b)=>{
-        if( a.payment > b.payment ){
-            return -1;
-        }else if( a.payment < b.payment ){
-            return 1;
-        } else{
-            return 0;
-        }
-    });
-    return{takeMoneyMemberList:takeMoneyMemberList,giveMoneyMemberList:giveMoneyMemberList};
-
-}
+    
 
 
 const data_ = [{"member":"A" ,"payment":5000},
